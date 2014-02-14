650b88ef07b1a432131ab696601bc98e2ec9d640
ChrisJamesAllen <chris@jonny-lu.com>
2014-02-14 15:08:02 +0000

    stream page for aw14

--- app/scripts/views/app.js
+++ app/scripts/views/app.js
@@ -23,32 +23,11 @@ define([
         },
         render: function() {
             this.$el.html(this.template());
-            this.$video = this.$('.video');
             this.resize();
             return this;
         },
         resize: function() {
-            var aspect = 800 / 1920;
-            var w = _.min([$(window).width() * 0.75, 1000]);
-            w = _.max([w, 300]);
-            var h = w * aspect;
 
-
-
-            h = $(window).height() * 0.4;
-            w = h * (1920 / 800);
-
-            if (w >= ($(window).width() * 0.7)) {
-                w = _.min([$(window).width() * 0.75, 1000]);
-                h = w * aspect;
-                w = Math.ceil(w);
-                h = Math.ceil(h);
-            }
-
-            this.$video.css({
-                width: w,
-                height: h
-            });
         }
     });
 
